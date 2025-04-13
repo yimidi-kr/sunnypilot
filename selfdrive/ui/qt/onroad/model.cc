2da75359f84dad1360e5fba77fb48c46485984a2
rav4kumar <meetkumardesai@gmail.com>
2025-04-13 15:52:25 -0700

    blind spot ui

--- selfdrive/ui/qt/onroad/model.cc
+++ selfdrive/ui/qt/onroad/model.cc
@@ -74,6 +74,12 @@ void ModelRenderer::update_model(const cereal::ModelDataV2::Reader &model, const
     mapLineToPolygon(lane_lines[i], 0.025 * lane_line_probs[i], 0, &lane_line_vertices[i], max_idx);
   }
 
+  // update lane lines
+  int max_distance_barrier =  100;
+  int max_idx_barrier = std::min(max_idx, get_path_length_idx(lane_lines[0], max_distance_barrier));
+  mapLineToPolygon(model.getLaneLines()[1], 0.2, -0.05, &left_blindspot_vertices, max_idx_barrier);
+  mapLineToPolygon(model.getLaneLines()[2], 0.2, -0.05, &right_blindspot_vertices, max_idx_barrier);
+
   // update road edges
   const auto &road_edges = model.getRoadEdges();
   const auto &edge_stds = model.getRoadEdgeStds();
@@ -112,6 +118,16 @@ void ModelRenderer::drawPath(QPainter &painter, const cereal::ModelDataV2::Reade
 
   float v_ego = sm["carState"].getCarState().getVEgo();
   bool rainbow = Params().getBool("RainbowMode");
+  bool left_blindspot = sm["carState"].getCarState().getLeftBlindspot();
+  bool right_blindspot = sm["carState"].getCarState().getRightBlindspot();
+
+  painter.setBrush(QColor::fromRgbF(1.0, 0.0, 0.0, 0.4));  // Red with alpha for blind spot
+
+  if (left_blindspot && !left_blindspot_vertices.isEmpty())
+    painter.drawPolygon(left_blindspot_vertices);
+  if (right_blindspot && !right_blindspot_vertices.isEmpty())
+    painter.drawPolygon(right_blindspot_vertices);
+
 
   // Get the current time in seconds for dynamic effect (speed of rainbow movement)
   float time_offset = std::chrono::duration_cast<std::chrono::milliseconds>(
